5fb7a696378497009037b3221c583018c7fe5d56
Shivdas Pendor <shivdaspendorr750@gmail.com>
2021-02-10 14:20:10 +0530

    first

---
+++ pop.js
@@ -0,0 +1,18 @@
+
+    function getVal()
+    {
+        var txt = document.getElementById('')[0].value ;
+    }
+    function getAndSetVal()
+    {
+        var txt1 = document.getElementById('txt').value;
+      document.title =  document.getElementById('txt').value = txt1;
+       
+    }
+    function click()
+    {
+        document.getElementById("popup-box").classList.toggle("active");
+    }
+   
+    
+    
